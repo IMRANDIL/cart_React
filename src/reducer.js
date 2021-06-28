3fd8ce2566c3fa4f7c0a5873d7fdb078866d350e
IMRANDIL <aliimranadil2@gmail.com>
2021-06-28 17:06:51 +0530

    first commit

---
+++ src/reducer.js
@@ -0,0 +1,68 @@
+
+
+
+const reducer = (state, action) => {
+    if (action.type === 'CLEAR_CONTAINER') {
+        return { ...state, cart: [] }
+    }
+
+    if (action.type === 'REMOVE') {
+        return { ...state, cart: state.cart.filter((item) => item.id !== action.payload) }
+    }
+
+    if (action.type === 'INCREASE') {
+        let tempCart = state.cart.map((item) => {
+            if (item.id === action.payload) {
+                return { ...item, amount: item.amount + 1 }
+            }
+            return item;
+        });
+        return { ...state, cart: tempCart }
+    }
+
+
+    if (action.type === 'DECREASE') {
+        let tempCart = state.cart.map((item) => {
+            if (item.id === action.payload) {
+                return { ...item, amount: item.amount - 1 }
+            }
+
+            return item;
+        }).filter((selec)=>selec.amount !== 0);
+        return { ...state, cart: tempCart }
+    }
+
+
+    if(action.type === 'GET_TOTALS'){
+
+let {total,amount} = state.cart.reduce((cartTotal,cartItem)=>{
+    const {price,amount} = cartItem;
+    const itemTotal = price * amount
+    cartTotal.amount += amount;
+    cartTotal.total += itemTotal
+    return cartTotal;
+},{
+    total:0,
+    amount:0
+})
+
+total = parseFloat(total.toFixed(2))
+
+        return {...state,total,amount}
+    }
+
+
+    if(action.type === 'LOADING'){
+        return {...state,loading:true}
+    }
+
+
+    if(action.type === 'DISPLAY'){
+        return {...state,loading:false,cart:action.payload}
+    }
+
+
+    return state
+}
+
+export default reducer;
